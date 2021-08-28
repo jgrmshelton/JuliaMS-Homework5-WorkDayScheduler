8cc8d3dd73ea61e94f57e24ab59806f904f2b842
Julia Shelton <87336948+jgrmshelton@users.noreply.github.com>
2021-08-28 18:14:36 -0500

    commit

--- script.js
+++ script.js
@@ -1,56 +1,47 @@
-var todayDate = moment().format("dddd, MMM Do YYYY");
-$("#currentDay").html(todayDate);
+var currentDate;
+var dateText = "";
+var apptTime = "";
+var time;
+var today;
+var dateArray = [];
+var storedCal;
+var returnedCal;
 
-$(document).ready(function () {
-    //click listener - save button
-    $(".saveBtn").on("click", function () {
-        //values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+$(document).on("search", function () {
+    currentDate = moment().format("dddd MMM Do YYYY")
+    $("#currentDay").append(today);
+    time = moment().fomat("hr");
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    })
-   
-    function timeTracker() {
-        //get current number of hours
-        var currentTime = moment().hour();
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            // To check the time and add the classes for background indicators
-            if (blockTime < currentTime) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
+    function renderSchedule() {
+        storedCal = JSON.parse(localStorage.getItem("appts"));
+        if (storedCal !== 0) {
+            for (i = 0; i < storedCal.length; i++) {
+                returnedCal = storedCal[i];
+                details = returnedCal.details;
+                timeIndex = returnedCal.time;
+                timeIndex = timeIndex.replace(":00",'');
+                if (details !== null) {
+                    $("#" + timeIndex).children("div").children("div").children("textarea").val(details);
+                }
             }
-            else if (blockTime === currentTime) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-
-            }
-        })
+        }
     }
 
-    // Get item from local storage if any
-    $("#8AM .description").val(localStorage.getItem("8AM"));
-    $("#9AM .description").val(localStorage.getItem("9AM"));
-    $("#10AM .description").val(localStorage.getItem("10AM"));
-    $("#11AM .description").val(localStorage.getItem("11AM"));
-    $("#12PM .description").val(localStorage.getItem("12PM"));
-    $("#13PM .description").val(localStorage.getItem("13PM"));
-    $("#14PM .description").val(localStorage.getItem("14PM"));
-    $("#15PM .description").val(localStorage.getItem("15PM"));
-    $("#16PM .description").val(localStorage.getItem("16PM"));
-    $("#17PM .description").val(localStorage.getItem("17PM"));
+    renderSchedule();
 
-    timeTracker();
+    for (i = 0; i <= 23; i++) {
+        todayContainer = i;
+        if (time == i) {
+            $('#' + todayContainer).addClass("present");
+            $('#' + todayContainer).children('div').children('div').children("textarea").addClass("present");
+        }
+        else if (time > i) {
+            $('#' + todayContainer).addClass("past");
+            $('#' + todayContainer).children('div').children('div').children("textarea").addClass("past");
+        }
+        else {
+            $('#' + todayContainer).addClass("future");
+            $('#' + todayContainer).children('div').children('div').children("textarea").addClass("future");
+        }
+    }
 })
