a64655f4474d0b81f31461ed8aed688c30f73b8c
Julia Shelton <87336948+jgrmshelton@users.noreply.github.com>
2021-08-28 21:56:48 -0500

    commit

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ var todayDate = moment().format("HR");
 var todayDateInt = parseInt(todayDate);
 var saveBtn = $(".saveBtn");
 
-$("#8AM").attr("date-time", moment("8:00 AM", "h:mm a").format("HR"));
-$("#9AM").attr("date-time", moment("9:00 AM", "h:mm a").format("HR"));
-$("#10AM").attr("date-time", moment("10:00 AM", "h:mm a").format("HR"));
-$("#11AM").attr("date-time", moment("11:00 AM", "h:mm a").format("HR"));
-$("#12PM").attr("date-time", moment("12:00 PM", "h:mm a").format("HR"));
-$("#1PM").attr("date-time", moment("1:00 PM", "h:mm a").format("HR"));
-$("#2PM").attr("date-time", moment("2:00 PM", "h:mm a").format("HR"));
-$("#3PM").attr("date-time", moment("3:00 PM", "h:mm a").format("HR"));
-$("#4PM").attr("date-time", moment("4:00 PM", "h:mm a").format("HR"));
-$("#5PM").attr("date-time", moment("5:00 PM", "h:mm a").format("HR"));
+$("#1").attr("date-time", moment("8:00 AM", "h:mm a").format("HR"));
+$("#2").attr("date-time", moment("9:00 AM", "h:mm a").format("HR"));
+$("#3").attr("date-time", moment("10:00 AM", "h:mm a").format("HR"));
+$("#4").attr("date-time", moment("11:00 AM", "h:mm a").format("HR"));
+$("#5").attr("date-time", moment("12:00 PM", "h:mm a").format("HR"));
+$("#6").attr("date-time", moment("1:00 PM", "h:mm a").format("HR"));
+$("#7").attr("date-time", moment("2:00 PM", "h:mm a").format("HR"));
+$("#8").attr("date-time", moment("3:00 PM", "h:mm a").format("HR"));
+$("#9").attr("date-time", moment("4:00 PM", "h:mm a").format("HR"));
+$("#10").attr("date-time", moment("5:00 PM", "h:mm a").format("HR"));
 
 $(document).ready(function () {
     renderSchedule();
@@ -41,29 +41,27 @@ $(document).ready(function () {
     }
 
     saveBtn.on("click", function () {
-
+        console.log($(this).parent().parent().attr("id"))
         // set a variable to select the clicked-on-button's data-hour attribute which we set in the HTML
-        var hour = $(this).siblings(".hour").text()
-      
-
+        var hour = $(this).parent().parent().attr('id')
+       
         // set a variable to select the value of the user's "plan" (input) at a particular hour
         var plan = $(this).siblings("textarea").val()
-      
+        console.log($(this).siblings("textarea").val())
         // var plan = $("#" + hour + "Row").val();
-
+       
         // save the hour's plan to local storage
-        localStorage.setItem(hour, plan);
-
-
+            localStorage.setItem(hour, plan);
+       
+       
     });
 
     //  Function to retrieve stored user inputs from local storage and populate the hour's input value with them
     function renderSchedule() {
-        // hours - 12 hrs to account for
-        // starts at 1 because 1 is the lowest hour
-        for (var i = 1; i <= 12; i++) {
-            // select the 
-            $("#" + i + "Row").val(localStorage.getItem(i));
+        // 8 being the first hour so it's 1
+        for (var i = 1; i <= 1; i++) {
+             
+            $("#" + i + "Row").val(localStorage.getItem("1 2 3 4 5 6 7 8 9 10"));
         }
     }
 
